75b900c35a440d6436920c9a7f2b704f28a83dc3
Kelyn Mann <kelyndm@yahoo.com>
2017-04-03 03:09:00 -0400

    End Section 1

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,4 +7,12 @@ router
     .route('/hotels')
     .get(ctrlHotels.hotelsGetAll);
 
+router
+    .route('/hotels/:hotelId')
+    .get(ctrlHotels.hotelsGetOne);
+
+router
+    .route('/hotels/new')
+    .post(ctrlHotels.hotelsAddOne);
+
 module.exports = router;
