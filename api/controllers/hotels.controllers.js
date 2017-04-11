c7720bbb4fb98af964c73f811c508d8eae84ef6b
Kelyn Mann <kelyndm@yahoo.com>
2017-04-11 01:56:46 -0400

    Finish Vanilla MongoDB integration for API. No use of Mongoose with this version. Example of how to use basic MongoDB calls with NodeJS

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -46,9 +46,26 @@ module.exports.hotelsGetOne = function(req, res) {
 };
 
 module.exports.hotelsAddOne = function(req, res) {
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
+    var newHotel;
+
     console.log('POST new hotel');
-    console.log(req.body);
-    res
-        .status(200)
-        .json(req.body);
+
+    if(req.body && req.body.name && req.body.stars) {
+        newHotel = req.body;
+        newHotel.stars = parseInt(req.body.stars, 10);
+        collection.insertOne(newHotel, function(err, response) {
+            console.log(response);
+            console.log(response.ops);
+            res
+                .status(201)
+                .json(response.ops);
+        });
+        
+    } else {
+        res
+            .status(400)
+            .json({message: "Required data missing from body"});
+    }
 };
