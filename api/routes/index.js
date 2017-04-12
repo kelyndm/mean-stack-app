a97321c27823b63d8f02419bc159a5f1db2c59b9
Kelyn Mann <kelyndm@yahoo.com>
2017-04-12 09:48:00 -0400

    Add api support and restful routes for getting subdocuments from the hotels (reviews)

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var ctrlHotels = require('../controllers/hotels.controllers.js');
+var ctrlReviews = require('../controllers/reviews.controllers.js');
 
+//Hotel routes
 router
     .route('/hotels')
     .get(ctrlHotels.hotelsGetAll);
@@ -14,5 +16,14 @@ router
 router
     .route('/hotels/new')
     .post(ctrlHotels.hotelsAddOne);
+
+// Review routes
+router
+    .route('/hotels/:hotelId/reviews')
+    .get(ctrlReviews.reviewsGetAll);
+
+router
+    .route('/hotels/:hotelId/reviews/:reviewId')
+    .get(ctrlReviews.reviewsGetOne);
 
 module.exports = router;
