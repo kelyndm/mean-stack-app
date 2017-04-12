34e131ead68803d1375fb91e0a8861a940989f36
Kelyn Mann <kelyndm@yahoo.com>
2017-04-12 00:41:58 -0400

    Add advanced Schema properties and requirements as well as nested Schemas onto our hotel model

--- api/data/hotels.model.js
+++ api/data/hotels.model.js
@@ -1,5 +1,34 @@
 var mongoose = require('mongoose');
 
+var reviewSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 0,
+        max: 5,
+        "default": 0 //<- Put default in quotes because default is a javascript key word
+    },
+    review: {
+        type: String,
+        required: true
+    },
+    createdOn: {
+        type: Date,
+        "default": Date.now
+    }
+});
+
+var roomSchema = new mongoose.Schema({
+    type: String,
+    number: Number,
+    description: String,
+    photos: [String],
+    price: Number
+});
+
 var hotelSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -14,7 +43,18 @@ var hotelSchema = new mongoose.Schema({
     services: [String], //<-Says services is an array of strings
     description: String,
     photos: [String],
-    currency: String
+    currency: String,
+    reviews: [reviewSchema],
+    rooms: [roomSchema],
+    location: {
+        address: String,
+        // Always store coordinates in longitude (E/W), latitude (N/S) order
+        coordinates: {
+            type: [Number],
+            index: '2dsphere'
+        }
+    }
 });
 
-mongoose.model('Hotel', hotelSchema, 'hotels');
+//mongoose.model('Hotel', hotelSchema, 'hotels');
+mongoose.model('Hotel', hotelSchema); // <- You don't have to define collection of 'hotels' (3rd param) - mongoose does this by pluralizing the first param.. the name of the model
