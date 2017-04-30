a6d07b052aead2f385556fb8c2e5766264bcdb69
Kelyn Mann <kelyndm@yahoo.com>
2017-04-30 18:17:20 -0400

    Add single hotel view.. TODO Fix it.. the links are not working

--- public/angular-app/hotel-list/hotel-list-controller.js
+++ public/angular-app/hotel-list/hotel-list-controller.js
@@ -3,7 +3,7 @@ angular.module('meanhotel').controller('HotelsController', HotelsController);
 function HotelsController($http) {
     var vm = this;
     vm.title = 'MEAN Hotel App';
-    $http.get('/api/hotels?count=10').then(function(response) {
+    $http.get('/api/hotels?count=12').then(function(response) {
         // console.log(response);
         vm.hotels = response.data;
     });
